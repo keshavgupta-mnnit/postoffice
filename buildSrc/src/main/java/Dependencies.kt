973a530f54786ffe938216381e334ab2e7212c6d
Keshav <keshavgupta.mnnit@gmail.com>
2023-04-26 02:37:50 +0530

    initial commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,11 @@
+object Dependencies {
+
+    val coreKtx by lazy { "androidx.core:core-ktx:${Versions.coreKtx}" }
+    val appCompat by lazy { "androidx.appcompat:appcompat:${Versions.appCompat}" }
+    val material by lazy { "com.google.android.material:material:${Versions.material}" }
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
+    val junit by lazy { "junit:junit:${Versions.junit}" }
+    val junitExt by lazy { "androidx.test.ext:junit:${Versions.junitExt}" }
+    val espressoCore by lazy { "androidx.test.espresso:espresso-core:${Versions.espressoCore}" }
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+
+    const val coreKtx = "1.7.0"
+    const val appCompat = "1.6.1"
+    const val material = "1.8.0"
+    const val constraintLayout = "2.1.4"
+    const val junit = "4.13.2"
+    const val junitExt = "1.1.5"
+    const val espressoCore = "3.5.1"
+
+    const val compileSdk = 33
+    const val minSdk = 24
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
